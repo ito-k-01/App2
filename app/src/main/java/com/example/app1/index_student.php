b0b6ae95b6b83ae92af0ca8b8659298d7d8ba52a
ito-k-01 <68677365+ito-k-01@users.noreply.github.com>
2021-06-21 18:13:58 +0900

    Create index_student.php
    index_student.php

---
+++ app/src/main/java/com/example/app1/index_student.php
@@ -0,0 +1,29 @@
+<!-- ログインページ -->
+<!doctype html>
+<html lang="ja">
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
+	<title>担当者通知システム</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header"><span>学生用メインページ</span></div>
+			<div id="main">
+				<div id="menu">
+					<span>Menu</span>
+					<input type="button" id="calender_button" onclick="location.href='./calender.php'" value="カレンダーページ"><br>
+					<input type="button" id="verification_button" onclick="location.href='./verification.php'"value="今週の担当者確認ページ"><br>
+				</div>
+				<div id="tantou">
+					<span>今週のあなたの担当</span>
+					<div id="output"></div>
+				</div>
+			</div>
+		<div id="footer"><span>フッター</span></div>
+	</div>
+</body>
+</html>
